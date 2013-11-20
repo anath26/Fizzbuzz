98639cc86c8988b149aaccf3cfda888dbf7c65cc
anath <anathabensour@gmail.com>
2013-11-20 11:47:20 +0000

    initial commit

---
+++ Fizzbuzz.rb
@@ -0,0 +1,14 @@
+ def divisible_by_3?(number)
+	number % 3 == 0 
+end
+def divisible_by_5?(number)
+	number % 5 == 0 
+end
+
+def fizzbuzz(number)
+	return "fizzbuzz" if divisible_by_3?(number) && divisible_by_5?(number)
+	return "fizz" if divisible_by_3?(number)
+	return "buzz" if divisible_by_5?(number)
+	number
+end
+

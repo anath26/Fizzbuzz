1227ee039555d79f2654ab27ba3bff4e31f1fd9b
anath <anathabensour@gmail.com>
2013-11-22 12:53:11 +0000

    added

--- Fizzbuzz.rb
+++ Fizzbuzz.rb
@@ -5,10 +5,10 @@ def divisible_by_5?(number)
 	number % 5 == 0 
 end
 
-def fizzbuzz(number)
-	return "fizzbuzz" if divisible_by_3?(number) && divisible_by_5?(number)
-	return "fizz" if divisible_by_3?(number)
-	return "buzz" if divisible_by_5?(number)
-	number
-end
+# def fizzbuzz(number)
+# 	return "fizzbuzz" if divisible_by_3?(number) && divisible_by_5?(number)
+# 	return "fizz" if divisible_by_3?(number)
+# 	return "buzz" if divisible_by_5?(number)
+# 	number
+# end
 
